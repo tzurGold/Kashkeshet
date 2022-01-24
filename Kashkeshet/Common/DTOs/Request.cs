3ec7b9752a3711f20851085056e2e72e36ea0993
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-24 19:25:32 +0200

    Add Request DTO- the data that will be sent from client to server

---
+++ Kashkeshet/Common/DTOs/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Common.DTOs
+{
+    [Serializable]
+    public class Request
+    {
+        private readonly Chat _chat;
+        private readonly Message _message;
+
+        public Request(Chat chat, Message message)
+        {
+            _chat = chat;
+            _message = message;
+        }
+    }
+}
