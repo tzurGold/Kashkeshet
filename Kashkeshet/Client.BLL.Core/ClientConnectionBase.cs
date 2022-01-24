3c53e791bbf0b6c4992c08bfc311a9d30e5e9996
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-24 20:25:19 +0200

    Add connection abstraction for client

---
+++ Kashkeshet/Client.BLL.Core/ClientConnectionBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Client.BLL.Core
+{
+    public abstract class ClientConnectionBase
+    {
+        private readonly uint _port;
+        private readonly string _ip;
+
+        protected ClientConnectionBase(uint port, string ip)
+        {
+            _port = port;
+            _ip = ip;
+        }
+
+        public abstract void Connect();
+        public abstract void CloseConnection();
+    }
+}
