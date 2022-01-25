4be59eb58b356ac4a824c592a09de0da3735839c
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 15:58:26 +0200

    Add ClientOrchestrator

---
+++ Kashkeshet/Client.BLL.Implementation/KashkeshetClientOrchestrator.cs
@@ -0,0 +1,12 @@
+﻿using Client.BLL.Core;
+
+namespace Client.BLL.Implementation
+{
+    public class KashkeshetClientOrchestrator : IClientOrchestrator
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
