5a722e678c6877737771ccbf1c8767d1e9a74d81
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 11:07:22 +0200

    Add menu description implementation

---
+++ Kashkeshet/Client.UI.Implementation/KashkeshetMenuDescription.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Client.UI.Core;
+
+namespace Client.UI.Implementation
+{
+    public class KashkeshetMenuDescription : MenuDescriptionBase
+    {
+        public KashkeshetMenuDescription(IList<string> options) : base(options)
+        {
+
+        }
+
+        public override IDictionary<int, string> GetDescription()
+        {
+            int i = 0;
+            return Options.ToDictionary(option => ++i);
+        }
+    }
+}
