5f3be02d18ec1fd9b1c8940226fa4916eb7e2271
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 12:59:58 +0200

    Add common message abstraction

---
+++ Kashkeshet/Client.BLL.Core/CommonMessageReceiverBase.cs
@@ -0,0 +1,17 @@
+﻿using Client.UI.Core;
+using Common.DTOs;
+
+namespace Client.BLL.Core
+{
+    public abstract class CommonMessageReceiverBase : MessageReceiverBase
+    {
+        protected CommonMessageReceiverBase(IInputReceiver inputReceiver) : base(inputReceiver)
+        {
+        }
+
+        public override Message GetMessage()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Kashkeshet/Client.BLL.Core/MessageReceiverBase.cs
@@ -0,0 +1,18 @@
+﻿using Client.UI.Core;
+using Common.DTOs;
+
+namespace Client.BLL.Core
+{
+    public abstract class MessageReceiverBase
+    {
+        protected readonly IInputReceiver InputReceiver;
+
+        protected MessageReceiverBase(IInputReceiver inputReceiver)
+        {
+            InputReceiver = inputReceiver;
+        }
+
+        public abstract Message GetMessage();
+        protected abstract string ChooseMessageRecipients();
+    }
+}
