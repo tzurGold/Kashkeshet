38635c982bd38083dc26f4f12c8bbc482fb7ae10
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 11:33:50 +0200

    Create client console app

---
+++ Kashkeshet/KashkeshetConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KashkeshetConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
