d8b1ae944c208077a0f519c54f433e3613ad1062
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 09:09:35 +0200

    Add communicator implementation for tcp

---
+++ Kashkeshet/Common/Communicators/Implementations/TcpCommunicator.cs
@@ -0,0 +1,37 @@
+﻿using Common.Communicators.Abstractions;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+
+namespace Common.Communicators.Implementations
+{
+    public class TcpCommunicator : ICommunicator
+    {
+        private TcpClient _client;
+        private NetworkStream _clientStream;
+        private IFormatter _formatter;
+
+        public TcpCommunicator(TcpClient client, IFormatter formatter)
+        {
+            _client = client;
+            _formatter = formatter;
+            Initializer();
+        }
+
+        public object Receive()
+        {
+            object obj = _formatter.Deserialize(_clientStream);
+            return obj;
+        }
+
+        public void Send(object obj)
+        {
+            _formatter.Serialize(_clientStream, obj);
+        }
+
+        private void Initializer()
+        {
+            _clientStream = _client.GetStream();
+        }
+
+    }
+}
