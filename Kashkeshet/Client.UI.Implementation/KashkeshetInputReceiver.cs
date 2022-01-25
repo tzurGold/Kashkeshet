e1d8704ee18db45a8a769919d7fd209938ea1c81
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 11:25:06 +0200

    Add input receiver implementation

---
+++ Kashkeshet/Client.UI.Implementation/KashkeshetInputReceiver.cs
@@ -0,0 +1,18 @@
+﻿using Client.UI.Core;
+using Common.IO;
+
+namespace Client.UI.Implementation
+{
+    public class KashkeshetInputReceiver : IInputReceiver
+    {
+        private readonly IInput<string> _reader;
+        private readonly IOutput<string> _writer;
+
+        public string GetInput(string messageToUser)
+        {
+            _writer.Write(messageToUser);
+            string input = _reader.Read();
+            return input;
+        }
+    }
+}
