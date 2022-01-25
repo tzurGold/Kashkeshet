f310a40051bb3e6ce567c8ac12d0181052b242b6
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-25 11:50:04 +0200

    Implement request factory

---
+++ Kashkeshet/Client.BLL.Implementation/KashkeshetRequestFactory.cs
@@ -0,0 +1,13 @@
+﻿using Client.BLL.Core;
+using Common.DTOs;
+
+namespace Client.BLL.Implementation
+{
+    public class KashkeshetRequestFactory : IRequestFactory
+    {
+        public Request CreateRequest(RequestType requestType, Message message, string fromWho)
+        {
+            return new Request(requestType, message, fromWho);
+        }
+    }
+}
