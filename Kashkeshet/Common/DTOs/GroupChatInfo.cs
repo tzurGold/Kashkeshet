7b23f2e6ee5275a75de146967e8329b0906a7405
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-26 20:58:44 +0200

    Add GroupChatInfo DTO

---
+++ Kashkeshet/Common/DTOs/GroupChatInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.DTOs
+{
+    [Serializable]
+    public class GroupChatInfo
+    {
+        public readonly bool IsVolatile;
+        public readonly IList<string> Participants;
+
+        public GroupChatInfo(bool isVolatile, IList<string> participants)
+        {
+            IsVolatile = isVolatile;
+            Participants = participants;
+        }
+    }
+}
