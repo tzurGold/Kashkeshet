7c7af2e04ac193f459acc4edacc7cc10b6676ad7
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-26 11:46:59 +0200

    Add IMemberSelector abstraction

---
+++ Kashkeshet/Server.BLL.Core/IMembersSelector.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Server.BLL.Core
+{
+    public interface IMembersSelector
+    {
+        IList<string> GetMembersNames(string to);
+    }
+}
---
+++ Kashkeshet/Server.BLL.Implementation/RequestHandlers/SendMessageRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Common.Communicators.Abstractions;
+using Common.DTOs;
+using Server.BLL.Core;
+using Server.BLL.Core.Chats;
+
+namespace Server.BLL.Implementation.RequestHandlers
+{
+    public class SendMessageRequestHandler : RequestHandlerBase
+    {
+        private readonly IConnectionsSelector _connectionsSelector;
+
+        public SendMessageRequestHandler(IResponseFactory responseFactory,
+            IResponseSender responseSender,
+            IConnectionsSelector connectionsSelector)
+            : base(responseFactory, responseSender)
+        {
+            _connectionsSelector = connectionsSelector;
+        }
+
+        public override void HandleRequest(Request request,
+            IDictionary<string, ICommunicator> connections,
+            IList<IChat> chats)
+        {
+            
+            Response response = ResponseFactory.CreateResponse(request.From,
+                request.ClientMessage.Content,
+                request.ClientMessage.ContentType);
+            
+            IList<string> members = request.ClientMessage.To
+          
+            //var recipientsConnections = _connectionsSelector.GetRecipientsCommunicators(connections, members);
+            //ResponseSender.SendResponse(response, recipientsConnections);
+        }
+    }
+}
