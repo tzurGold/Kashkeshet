bf27d04b044014b0bfe626ea51cc60c36e706604
Tzur Goldstein <tzurgold10@gmail.com>
2022-01-27 14:16:46 +0200

    Add configuration to common layer

---
+++ Kashkeshet/Common/SocketParamaterReciever.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Net;
+
+namespace PingPong.Common
+{
+    public class SocketParamaterReciever
+    {
+        public string GetSocketIPAddress()
+        {
+            string ip = ConfigurationManager.AppSettings["ip"];
+            return ip;
+        }
+
+        public int GetSocketPort()
+        {
+            int port = int.Parse(ConfigurationManager.AppSettings["port"]);
+            return port;
+        }
+    }
+}
